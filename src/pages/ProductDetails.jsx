455782bcb792d93b157850d4487b3236301e594b
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2022-12-09 11:12:13 +0700

    vi2__1.04.30: user rating

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { useParams } from 'react-router-dom'
 import products from '../assets/data/products'
@@ -6,16 +6,20 @@ import '../styles/product-details.css'
 import Helmet from '../components/Helmet/Helmet'
 import CommonSection from '../components/UI/CommonSection'
 import products from '../assets/data/products'
+import { motion } from 'framer-motion'
 
 const ProductDetails = () => {
+
+    const [tab, setTab] = useState('desc')
+
     const { id } = useParams()
     const product = products.find((item) => item.id === id)
 
-    const { imgUrl, price, productName, avgRating, review, description, shortDesc } = product;
+    const { imgUrl, price, productName, avgRating, reviews, description, shortDesc } = product;
 
     return (
-        <Helmet>
-            <CommonSection />
+        <Helmet title={productName}>
+            <CommonSection title={productName} />
 
             <section className='pt-0'>
                 <Container>
@@ -38,10 +42,70 @@ const ProductDetails = () => {
                                 </div>
                                 <span className='product__price'>{price} VND.</span>
                                 <p className='mt-3'>{shortDesc}</p>
-                                <button className='buy__btn'>
+                                <motion.button whileTap={{ scale: 1.2 }} className='buy__btn'>
                                     Thêm vào Giỏ hàng
-                                </button>
+                                </motion.button>
                             </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='12' >
+                            <div className="tab__wrapper d-flex align-items-center gap-5">
+                                <h6 className={`${tab === 'desc' ? 'active__tab' : ''}`} onClick={() => setTab('desc')}>Chi tiết sản phẩm</h6>
+                                <h6 className={`${tab === 'rev' ? 'active__tab' : ''}`} onClick={() => setTab('rev')}>Đánh giá ({reviews.length})</h6>
+                            </div>
+
+                            {
+                                tab === 'desc' ? (<div className="tab__content mt-5">
+                                    <p>{description}</p>
+                                </div>)
+                                    : (<div className='product__review mt-5'>
+                                        <div className="review__wrapper">
+                                            <ul>
+                                                {
+                                                    reviews?.map((item, index) => (
+                                                        <li kew={index} className='mb-4'>
+
+                                                            <h6>
+                                                                Đức Toàn (User)
+                                                            </h6>
+                                                            <span>{item.rating} sao</span>
+                                                            <p>{item.text}</p>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+
+                                            <div className="review__form">
+                                                <h4>Để lại đánh giá (Feedback)</h4>
+                                                <form action="">
+                                                    <div className="form__group">
+                                                        <input type="text" placeholder='Enter name' />
+                                                    </div>
+
+                                                    <div className="form__group">
+                                                        <span>1 <i class="ri-star-s-fill"></i></span>
+                                                        <span>2 <i class="ri-star-s-fill"></i></span>
+                                                        <span>3 <i class="ri-star-s-fill"></i></span>
+                                                        <span>4 <i class="ri-star-s-fill"></i></span>
+                                                        <span>5 <i class="ri-star-s-fill"></i></span>
+                                                    </div>
+
+                                                    <div className="form__group">
+                                                        <textarea rows={4} type="text" placeholder='Review Message..' />
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>)
+                            }
+
                         </Col>
                     </Row>
                 </Container>
